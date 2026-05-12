27f6f165cfc124adeeb2b5b946d581ca025a0fb4
wxlwyy <3325904810@qq.com>
2026-05-12 21:12:48 +0800

    MCP服务端的图片搜索服务

---
+++ image-search-mcp-server/src/test/java/com/wxl/imagesearchmcpserver/tools/ImageSearchToolTest.java
@@ -0,0 +1,21 @@
+package com.wxl.imagesearchmcpserver.tools;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class ImageSearchToolTest {
+
+    @Resource
+    private ImageSearchTool imageSearchTool;
+
+    @Test
+    void searchImage() {
+        String result = imageSearchTool.searchImage("cat");
+        Assertions.assertNotNull(result);
+    }
+}
